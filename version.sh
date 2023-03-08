3e478f46871564ee27b56009263858260840151e
mbido <ma.bidault39@gmail.com>
2023-03-08 14:27:10 +0100

    creation fonction help et son appel

---
+++ version.sh
@@ -0,0 +1,38 @@
+#!/bin/dash
+
+help(){
+	echo 'Usage:
+\t./version.sh --help
+\t./version.sh <command> FILE [OPTION]
+\t\twhere <command> can be: add amend checkout|co commit|ci diff log reset rm
+
+./version.sh add FILE MESSAGE
+\tAdd FILE under versioning with the initial log message MESSAGE
+
+./version.sh commit|ci FILE MESSAGE
+\tCommit a new version of FILE with the log message MESSAGE
+
+./version.sh amend FILE MESSAGE
+\tModify the last registered version of FILE, or (inclusive) its log message
+
+./version.sh checkout|co FILE [NUMBER]
+\tRestore FILE in the version NUMBER indicated, or in the
+\tlatest version if there is no number passed in argument
+
+./version.sh diff FILE
+\tDisplays the difference between FILE and the last committed version
+
+./version.sh log FILE
+\tDisplays the logs of the versions already committed
+
+./version.sh reset FILE NUMBER
+\tRestores FILE in the version NUMBER indicated and
+\tdeletes the versions of number strictly superior to NUMBER
+
+./version.sh rm FILE
+\tDeletes all versions of a file under versioning'
+}
+
+if test $# -eq 1 -a "$1" = "--help";then
+	help
+fi
